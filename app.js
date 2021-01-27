b630ade85586e822919ac2bbb392aaa193560755
rvasquez <rvasquez1@gmail.com>
2021-01-26 22:53:39 -0500

    added inquirer

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const logo = require("asciiart-logo");
-const db = require("./db")
-require("console.table");
+const db = require("./db");
+//require("console.table");
 
 
 init();
@@ -9,5 +9,34 @@ init();
 function init(){
     const logoText = logo({name: "Rafa Employee Manager"}).render();
     console.log(logoText);
+    loadMainMenu();
+}
+
+function loadMainMenu(){
+    inquirer.prompt([
+        { 
+            type: "list",
+            name: "choice",
+            message: "What would you like to do??",
+            choices: [
+                {
+                    name: "View all Employees",
+                    value: "VIEW_EMPLOYEES"
+                },
+                {
+                    name: "View all Roles",
+                    value: "VIEW_ROLES"
+                }
+            ]
+        }
+
+]).then((response) => {
+    
+
+
+
+    
+  });
+
 
 }
