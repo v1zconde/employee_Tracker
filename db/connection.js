8035746ed1250d89106831c34a234b1425a9c9c8
rvasquez <rvasquez1@gmail.com>
2021-01-26 21:28:11 -0500

    added the modules, mysql, inquirer

--- db/connection.js
+++ db/connection.js
@@ -1,2 +1,14 @@
 const util = require("util");
 const mysql = require("mysql")
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "rootroot",
+    database: "employeeDB"
+})
+
+
+connection.connect();
+
+connection.query = util.promisify(connection.query, console.log("connected"));
